5e7a16e86767afd7c0aad77ccbb40d94fcd60676
luow <429175116@qq.com>
2019-02-26 19:55:59 +0800

    加载数据渲染图表流程

--- dowhere_app/pages/projectAll/projectAll.js
+++ dowhere_app/pages/projectAll/projectAll.js
@@ -2,9 +2,12 @@
 import * as echarts from '../../ec-canvas/echarts';
 
 const app = getApp();
-
-// 计划完成度--饼
-function setOptionPlanPie(chart) {
+function pieShow(data, chart) {
+  data = [
+    {"name": "产品1", "value": 100},
+    {"name": "产品2", "value": 30},
+    {"name": "产品3", "value": 80},
+  ]
   const option = {
     backgroundColor: "#ffffff",
     color: ["#37A2DA", "#32C5E9", "#67E0E3", "#91F2DE", "#FFDB5C", "#FF9F7F"],
@@ -16,24 +19,8 @@ function setOptionPlanPie(chart) {
       },
       type: 'pie',
       center: ['50%', '50%'],
-      radius: [0, '60%'],
-      data: [{
-        value: 55,
-        name: '北京55'
-      }, {
-        value: 20,
-        name: '武汉'
-      }, {
-        value: 10,
-        name: '杭州'
-      }, {
-        value: 20,
-        name: '广州'
-      }, {
-        value: 38,
-        name: '上海'
-      },
-      ],
+      radius: [0, '80%'],
+      data: data,
       itemStyle: {
         emphasis: {
           shadowBlur: 10,
@@ -44,168 +31,214 @@ function setOptionPlanPie(chart) {
     }]
   };
   chart.setOption(option);
+}
+function barShow(data, chart) {
+  let chartData = [
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50}
+  ]
+  // 产品名，计划，进度，剩余
+  let namelist = []
+  let planlist = []
+  let schedulelist = []
+  let remainderlist = []
+  let i = 0
+  for (i in chartData) {
+    namelist.push(chartData[i].name)
+    planlist.push(chartData[i].plan)
+    schedulelist.push(chartData[i].schedule)
+    remainderlist.push(chartData[i].remainder)
+  }
+  const option = {
+    color: ['#37a2da', '#32c5e9', '#67e0e3'],
+    // 控制浮动框的显示
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+        type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+      }
+    },
+    legend: {
+      data: ['计划', '完成', '剩余']
+    },
+    grid: {
+      left: 20,
+      right: 20,
+      bottom: 15,
+      top: 40,
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'value',
+        axisLine: {
+          lineStyle: {
+            color: '#999'
+          }
+        },
+        axisLabel: {
+          color: '#666'
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: 'category',
+        axisTick: { show: false },
+        data: namelist,
+        axisLine: {
+          lineStyle: {
+            color: '#999'
+          }
+        },
+        axisLabel: {
+          color: '#666'
+        }
+      }
+    ],
+    series: [
+      {
+        name: '计划',
+        type: 'bar',
+        label: {
+          normal: {
+            show: true,
+            position: 'inside'
+          }
+        },
+        data: planlist
+      },
+      {
+        name: '完成',
+        type: 'bar',
+        stack: '总量',
+        label: {
+          normal: {
+            show: true
+          }
+        },
+        data: schedulelist
+      },
+      {
+        name: '剩余',
+        type: 'bar',
+        stack: '总量',
+        label: {
+          normal: {
+            show: true,
+            position: 'left'
+          }
+        },
+        data: remainderlist
+      }
+    ]
+  };
+  chart.setOption(option);
+}
+// 计划完成度--饼
+function setOptionPlanPie(chart, time) {
+  if (time == "month") {
+    //加载月份数据---此处修改参数
+    
+  } else {
+    // 加载年份数据---此处修改参数
+  }
+  let data = null
+  // 图表渲染
+  pieShow(data, chart)
+  // wx.request({
+  //   url: `${this.$parent.globalData.requestUrl}/api/getData`,
+  //   method: 'POST',
+  //   data: {
+  //     userName: this.userName,
+  //     userPaw: this.userPaw
+  //   },
+  //   success: data => {
+  //     if (data.data.success) {
+  //       // data = data.data.novels
+        
+  //     } else {
+  //       wx.showModal({
+  //         title: '',
+  //         content: data.data.errmsg
+  //       })
+  //     }
+  //   }
+  // })
 }
 // 各区金额占比--饼
-function setOptionQuantityPie(chart) {
-  const option = {
-    backgroundColor: "#ffffff",
-    color: ["#37A2DA", "#32C5E9", "#67E0E3", "#91F2DE", "#FFDB5C", "#FF9F7F"],
-    series: [{
-      label: {
-        normal: {
-          fontSize: 14
-        }
-      },
-      type: 'pie',
-      center: ['50%', '50%'],
-      radius: [0, '60%'],
-      data: [{
-        value: 55,
-        name: '北京55'
-      }, {
-        value: 20,
-        name: '武汉'
-      }, {
-        value: 10,
-        name: '杭州'
-      }, {
-        value: 20,
-        name: '广州'
-      }, {
-        value: 38,
-        name: '上海'
-      },
-      ],
-      itemStyle: {
-        emphasis: {
-          shadowBlur: 10,
-          shadowOffsetX: 0,
-          shadowColor: 'rgba(0, 2, 2, 0.3)'
-        }
-      }
-    }]
-  };
-  chart.setOption(option);
+function setOptionQuantityPie(chart, time) {
+  if (time == "month") {
+    //加载月份数据---此处修改参数
+    
+  } else {
+    // 加载年份数据---此处修改参数
+  }
+  let data = null
+  // 图表渲染
+  pieShow(data, chart)
+  // wx.request({
+  //   url: `${this.$parent.globalData.requestUrl}/api/getData`,
+  //   method: 'POST',
+  //   data: {
+  //     userName: this.userName,
+  //     userPaw: this.userPaw
+  //   },
+  //   success: data => {
+  //     if (data.data.success) {
+  //       // data = data.data.novels
+        
+  //     } else {
+  //       wx.showModal({
+  //         title: '',
+  //         content: data.data.errmsg
+  //       })
+  //     }
+  //   }
+  // })
 }
 // 完成数据详情--柱
-function setOptionPlanBar(chart) {
-  let chartData = [
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50}
-]
-// 产品名，计划，进度，剩余
-let namelist = []
-let planlist = []
-let schedulelist = []
-let remainderlist = []
-let i = 0
-for (i in chartData) {
-  namelist.push(chartData[i].name)
-  planlist.push(chartData[i].plan)
-  schedulelist.push(chartData[i].schedule)
-  remainderlist.push(chartData[i].remainder)
-}
-console.log(namelist)
-console.log(planlist)
-console.log(schedulelist)
-console.log(remainderlist)
-const option = {
-  color: ['#37a2da', '#32c5e9', '#67e0e3'],
-  // 控制浮动框的显示
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-      type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-    }
-  },
-  legend: {
-    data: ['计划', '完成', '剩余']
-  },
-  grid: {
-    left: 20,
-    right: 20,
-    bottom: 15,
-    top: 40,
-    containLabel: true
-  },
-  xAxis: [
-    {
-      type: 'value',
-      axisLine: {
-        lineStyle: {
-          color: '#999'
-        }
-      },
-      axisLabel: {
-        color: '#666'
-      }
-    }
-  ],
-  yAxis: [
-    {
-      type: 'category',
-      axisTick: { show: false },
-      data: namelist,
-      axisLine: {
-        lineStyle: {
-          color: '#999'
-        }
-      },
-      axisLabel: {
-        color: '#666'
-      }
-    }
-  ],
-  series: [
-    {
-      name: '计划',
-      type: 'bar',
-      label: {
-        normal: {
-          show: true,
-          position: 'inside'
-        }
-      },
-      data: planlist
-    },
-    {
-      name: '完成',
-      type: 'bar',
-      stack: '总量',
-      label: {
-        normal: {
-          show: true
-        }
-      },
-      data: schedulelist
-    },
-    {
-      name: '剩余',
-      type: 'bar',
-      stack: '总量',
-      label: {
-        normal: {
-          show: true,
-          position: 'left'
-        }
-      },
-      data: remainderlist
-    }
-  ]
-};
-chart.setOption(option);
+function setOptionPlanBar(chart, time) {
+  if (time == "month") {
+    //加载月份数据---此处修改参数
+    
+  } else {
+    // 加载年份数据---此处修改参数
+  }
+  let data = null
+  // 图表渲染
+  barShow(data, chart)
+  // wx.request({
+  //   url: `${this.$parent.globalData.requestUrl}/api/getData`,
+  //   method: 'POST',
+  //   data: {
+  //     userName: this.userName,
+  //     userPaw: this.userPaw
+  //   },
+  //   success: data => {
+  //     if (data.data.success) {
+  //       // data = data.data.novels
+        
+  //     } else {
+  //       wx.showModal({
+  //         title: '',
+  //         content: data.data.errmsg
+  //       })
+  //     }
+  //   }
+  // })
 }
 
 Page({
@@ -214,7 +247,7 @@ Page({
     this.quantityPie = this.selectComponent('#quantity-pie');
     this.planPie = this.selectComponent('#plan-pie');
     this.planBar = this.selectComponent('#plan-bar');
-    this.init()
+    this.init(this.data.time)
   },
 
   data: {
@@ -252,7 +285,8 @@ Page({
         {"projectName": "项目6","id": "1"},
         {"projectName": "项目7","id": "1"}
       ]}
-    ]
+    ],
+    time: 'month'
   },
   onShow() {
 
@@ -260,11 +294,21 @@ Page({
   // 数据展示时间切换
   // 切换--年
   yearData(){
-    // getBarOption(chartData)
+    if (this.data.time === "month") {
+      this.data.time = "year"
+    } else {
+      return ''
+    }
+    this.init(this.data.time)
   },
   // 切换--月
   monthData(){
-    // getBarOption(chartData)
+    if (this.data.time === "year") {
+      this.data.time = "month"
+    } else {
+      return ''
+    }
+    this.init(this.data.time)
   },
 
   // 加载数据图表数据
@@ -330,7 +374,7 @@ Page({
     })
   },
   // 点击按钮后初始化图表
-  init() {
+  init(time) {
     this.quantityPie.init((canvas, width, height) => {
       // 获取组件的 canvas、width、height 后的回调函数
       // 在这里初始化图表
@@ -338,16 +382,7 @@ Page({
         width: width,
         height: height
       });
-      setOptionQuantityPie(chart);
-
-      // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
-      this.chart = chart;
-
-      this.setData({
-        isLoaded: true,
-        isDisposed: false
-      });
-
+      setOptionQuantityPie(chart, time);
       // 注意这里一定要返回 chart 实例，否则会影响事件处理等
       return chart;
     });
@@ -358,16 +393,7 @@ Page({
         width: width,
         height: height
       });
-      setOptionPlanPie(chart);
-
-      // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
-      this.chart = chart;
-
-      this.setData({
-        isLoaded: true,
-        isDisposed: false
-      });
-
+      setOptionPlanPie(chart, time);
       // 注意这里一定要返回 chart 实例，否则会影响事件处理等
       return chart;
     });
@@ -378,16 +404,13 @@ Page({
         width: width,
         height: height
       });
-      setOptionPlanBar(chart);
-
+      setOptionPlanBar(chart, time);
       // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
-      this.chart = chart;
-
-      this.setData({
-        isLoaded: true,
-        isDisposed: false
-      });
-
+      // this.chart = chart;
+      // this.setData({
+      //   isLoaded: true,
+      //   isDisposed: false
+      // });
       // 注意这里一定要返回 chart 实例，否则会影响事件处理等
       return chart;
     });
