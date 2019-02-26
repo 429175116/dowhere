9fe6403f6aa675eab8003a565e469b11956abbec
luow <429175116@qq.com>
2019-02-26 19:22:27 +0800

    图表延迟模式加载

--- dowhere_app/pages/projectAll/projectAll.js
+++ dowhere_app/pages/projectAll/projectAll.js
@@ -1,49 +1,237 @@
-// pages/projectAll/projectAll.js
+// // pages/projectAll/projectAll.js
 import * as echarts from '../../ec-canvas/echarts';
+
+const app = getApp();
+
+// 计划完成度--饼
+function setOptionPlanPie(chart) {
+  const option = {
+    backgroundColor: "#ffffff",
+    color: ["#37A2DA", "#32C5E9", "#67E0E3", "#91F2DE", "#FFDB5C", "#FF9F7F"],
+    series: [{
+      label: {
+        normal: {
+          fontSize: 14
+        }
+      },
+      type: 'pie',
+      center: ['50%', '50%'],
+      radius: [0, '60%'],
+      data: [{
+        value: 55,
+        name: '北京55'
+      }, {
+        value: 20,
+        name: '武汉'
+      }, {
+        value: 10,
+        name: '杭州'
+      }, {
+        value: 20,
+        name: '广州'
+      }, {
+        value: 38,
+        name: '上海'
+      },
+      ],
+      itemStyle: {
+        emphasis: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 2, 2, 0.3)'
+        }
+      }
+    }]
+  };
+  chart.setOption(option);
+}
+// 各区金额占比--饼
+function setOptionQuantityPie(chart) {
+  const option = {
+    backgroundColor: "#ffffff",
+    color: ["#37A2DA", "#32C5E9", "#67E0E3", "#91F2DE", "#FFDB5C", "#FF9F7F"],
+    series: [{
+      label: {
+        normal: {
+          fontSize: 14
+        }
+      },
+      type: 'pie',
+      center: ['50%', '50%'],
+      radius: [0, '60%'],
+      data: [{
+        value: 55,
+        name: '北京55'
+      }, {
+        value: 20,
+        name: '武汉'
+      }, {
+        value: 10,
+        name: '杭州'
+      }, {
+        value: 20,
+        name: '广州'
+      }, {
+        value: 38,
+        name: '上海'
+      },
+      ],
+      itemStyle: {
+        emphasis: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 2, 2, 0.3)'
+        }
+      }
+    }]
+  };
+  chart.setOption(option);
+}
+// 完成数据详情--柱
+function setOptionPlanBar(chart) {
+  let chartData = [
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
+  {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50}
+]
+// 产品名，计划，进度，剩余
+let namelist = []
+let planlist = []
+let schedulelist = []
+let remainderlist = []
+let i = 0
+for (i in chartData) {
+  namelist.push(chartData[i].name)
+  planlist.push(chartData[i].plan)
+  schedulelist.push(chartData[i].schedule)
+  remainderlist.push(chartData[i].remainder)
+}
+console.log(namelist)
+console.log(planlist)
+console.log(schedulelist)
+console.log(remainderlist)
+const option = {
+  color: ['#37a2da', '#32c5e9', '#67e0e3'],
+  // 控制浮动框的显示
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+      type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+    }
+  },
+  legend: {
+    data: ['计划', '完成', '剩余']
+  },
+  grid: {
+    left: 20,
+    right: 20,
+    bottom: 15,
+    top: 40,
+    containLabel: true
+  },
+  xAxis: [
+    {
+      type: 'value',
+      axisLine: {
+        lineStyle: {
+          color: '#999'
+        }
+      },
+      axisLabel: {
+        color: '#666'
+      }
+    }
+  ],
+  yAxis: [
+    {
+      type: 'category',
+      axisTick: { show: false },
+      data: namelist,
+      axisLine: {
+        lineStyle: {
+          color: '#999'
+        }
+      },
+      axisLabel: {
+        color: '#666'
+      }
+    }
+  ],
+  series: [
+    {
+      name: '计划',
+      type: 'bar',
+      label: {
+        normal: {
+          show: true,
+          position: 'inside'
+        }
+      },
+      data: planlist
+    },
+    {
+      name: '完成',
+      type: 'bar',
+      stack: '总量',
+      label: {
+        normal: {
+          show: true
+        }
+      },
+      data: schedulelist
+    },
+    {
+      name: '剩余',
+      type: 'bar',
+      stack: '总量',
+      label: {
+        normal: {
+          show: true,
+          position: 'left'
+        }
+      },
+      data: remainderlist
+    }
+  ]
+};
+chart.setOption(option);
+}
+
 Page({
+  onReady() {
+    // 获取组件
+    this.quantityPie = this.selectComponent('#quantity-pie');
+    this.planPie = this.selectComponent('#plan-pie');
+    this.planBar = this.selectComponent('#plan-bar');
+    this.init()
+  },
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-    ecBar: {
-      onInit: function (canvas, width, height) {
-        const barChart = echarts.init(canvas, null, {
-          width: width,
-          height: height
-        });
-        canvas.setChart(barChart);
-        barChart.setOption(getBarOption());
-
-        return barChart;
-      }
+    ecQuantityPie: {
+      // 将 lazyLoad 设为 true 后，需要手动初始化图表
+      lazyLoad: true 
     },
-
-    ecScatter: {
-      onInit: function (canvas, width, height) {
-        const scatterChart = echarts.init(canvas, null, {
-          width: width,
-          height: height
-        });
-        canvas.setChart(scatterChart);
-        scatterChart.setOption(getScatterOption());
-
-        return scatterChart;
-      }
+    ecPlanPie: {
+      // 将 lazyLoad 设为 true 后，需要手动初始化图表
+      lazyLoad: true 
     },
-
-    ecPie: {
-      onInit: function (canvas, width, height) {
-        const scatterChart = echarts.init(canvas, null, {
-          width: width,
-          height: height
-        });
-        canvas.setChart(scatterChart);
-        scatterChart.setOption(geTPieOption());
-
-        return scatterChart;
-      }
+    ecPlanBar: {
+      // 将 lazyLoad 设为 true 后，需要手动初始化图表
+      lazyLoad: true 
     },
+    isLoaded: false,
+    isDisposed: false,
     userId: null,
     projectListData: [
       {"name": "一区", "project": [
@@ -64,41 +252,8 @@ Page({
         {"projectName": "项目6","id": "1"},
         {"projectName": "项目7","id": "1"}
       ]}
-    ],
-    selGoPage_x: 0,
-    selGoPage_y: 0
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-    // this.setData({
-    //   userId: options.id
-    // })
-    
-    this.data.userId = '1'
-    this.getProjectList()
-    this.monthData()
-    // 获取权限之内的项目
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  // onReady() {
-  //   this.quantityPie = this.selectComponent('#mychart-dom-bar');
-  //   this.planPie = this.selectComponent('#mychart-dom-bar');
-  //   this.planBar = this.selectComponent('#mychart-dom-bar');
-  // },
-  // <!-- 总数量-饼 -->
-  // <ec-canvas id="quantity-pie" canvas-id="quantity-pie" ec="{{ ecPie }}"></ec-canvas>
-  // <!-- 完成度-饼 -->
-  // <ec-canvas id="plan-pie" canvas-id="plan-pie" ec="{{ ecBar }}"></ec-canvas>
-  // <!-- 各个项目完成-柱 -->
-  // <ec-canvas id="plan-bar" canvas-id="plan-bar" ec="{{ ecScatter }}"></ec-canvas>
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow() {
 
   },
@@ -173,242 +328,68 @@ Page({
         }
       }
     })
+  },
+  // 点击按钮后初始化图表
+  init() {
+    this.quantityPie.init((canvas, width, height) => {
+      // 获取组件的 canvas、width、height 后的回调函数
+      // 在这里初始化图表
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      });
+      setOptionQuantityPie(chart);
+
+      // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
+      this.chart = chart;
+
+      this.setData({
+        isLoaded: true,
+        isDisposed: false
+      });
+
+      // 注意这里一定要返回 chart 实例，否则会影响事件处理等
+      return chart;
+    });
+    this.planPie.init((canvas, width, height) => {
+      // 获取组件的 canvas、width、height 后的回调函数
+      // 在这里初始化图表
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      });
+      setOptionPlanPie(chart);
+
+      // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
+      this.chart = chart;
+
+      this.setData({
+        isLoaded: true,
+        isDisposed: false
+      });
+
+      // 注意这里一定要返回 chart 实例，否则会影响事件处理等
+      return chart;
+    });
+    this.planBar.init((canvas, width, height) => {
+      // 获取组件的 canvas、width、height 后的回调函数
+      // 在这里初始化图表
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      });
+      setOptionPlanBar(chart);
+
+      // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
+      this.chart = chart;
+
+      this.setData({
+        isLoaded: true,
+        isDisposed: false
+      });
+
+      // 注意这里一定要返回 chart 实例，否则会影响事件处理等
+      return chart;
+    });
   }
-})
-function geTPieOption() {
-  return {
-    backgroundColor: "#ffffff",
-    color: ["#37A2DA", "#32C5E9", "#67E0E3", "#91F2DE", "#FFDB5C", "#FF9F7F"],
-    series: [{
-      label: {
-        normal: {
-          fontSize: 14
-        }
-      },
-      type: 'pie',
-      center: ['50%', '50%'],
-      radius: [0, '60%'],
-      data: [{
-        value: 55,
-        name: '北京55'
-      }, {
-        value: 20,
-        name: '武汉'
-      }, {
-        value: 10,
-        name: '杭州'
-      }, {
-        value: 20,
-        name: '广州'
-      }, {
-        value: 38,
-        name: '上海'
-      },
-      ],
-      itemStyle: {
-        emphasis: {
-          shadowBlur: 10,
-          shadowOffsetX: 0,
-          shadowColor: 'rgba(0, 2, 2, 0.3)'
-        }
-      }
-    }]
-  };
-}
-function getBarOption() {
-  let chartData = [
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50},
-    {"name": "产品1", "plan": 100, "schedule": 50, "remainder": -50}
-  ]
-  // 产品名，计划，进度，剩余
-  let namelist = []
-  let planlist = []
-  let schedulelist = []
-  let remainderlist = []
-  let i = 0
-  for (i in chartData) {
-    namelist.push(chartData[i].name)
-    planlist.push(chartData[i].plan)
-    schedulelist.push(chartData[i].schedule)
-    remainderlist.push(chartData[i].remainder)
-  }
-  console.log(namelist)
-  console.log(planlist)
-  console.log(schedulelist)
-  console.log(remainderlist)
-  return {
-    color: ['#37a2da', '#32c5e9', '#67e0e3'],
-    // 控制浮动框的显示
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-        type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-      }
-    },
-    legend: {
-      data: ['计划', '完成', '剩余']
-    },
-    grid: {
-      left: 20,
-      right: 20,
-      bottom: 15,
-      top: 40,
-      containLabel: true
-    },
-    xAxis: [
-      {
-        type: 'value',
-        axisLine: {
-          lineStyle: {
-            color: '#999'
-          }
-        },
-        axisLabel: {
-          color: '#666'
-        }
-      }
-    ],
-    yAxis: [
-      {
-        type: 'category',
-        axisTick: { show: false },
-        data: namelist,
-        axisLine: {
-          lineStyle: {
-            color: '#999'
-          }
-        },
-        axisLabel: {
-          color: '#666'
-        }
-      }
-    ],
-    series: [
-      {
-        name: '计划',
-        type: 'bar',
-        label: {
-          normal: {
-            show: true,
-            position: 'inside'
-          }
-        },
-        data: planlist
-      },
-      {
-        name: '完成',
-        type: 'bar',
-        stack: '总量',
-        label: {
-          normal: {
-            show: true
-          }
-        },
-        data: schedulelist
-      },
-      {
-        name: '剩余',
-        type: 'bar',
-        stack: '总量',
-        label: {
-          normal: {
-            show: true,
-            position: 'left'
-          }
-        },
-        data: remainderlist
-      }
-    ]
-  };
-}
-
-function getScatterOption() {
-
-  var data = [];
-  var data2 = [];
-
-  for (var i = 0; i < 10; i++) {
-    data.push(
-      [
-        Math.round(Math.random() * 100),
-        Math.round(Math.random() * 100),
-        Math.round(Math.random() * 40)
-      ]
-    );
-    data2.push(
-      [
-        Math.round(Math.random() * 100),
-        Math.round(Math.random() * 100),
-        Math.round(Math.random() * 100)
-      ]
-    );
-  }
-
-  var axisCommon = {
-    axisLabel: {
-      textStyle: {
-        color: '#C8C8C8'
-      }
-    },
-    axisTick: {
-      lineStyle: {
-        color: '#fff'
-      }
-    },
-    axisLine: {
-      lineStyle: {
-        color: '#C8C8C8'
-      }
-    },
-    splitLine: {
-      lineStyle: {
-        color: '#C8C8C8',
-        type: 'solid'
-      }
-    }
-  };
-
-  return {
-    color: ["#FF7070", "#60B6E3"],
-    backgroundColor: '#eee',
-    xAxis: axisCommon,
-    yAxis: axisCommon,
-    legend: {
-      data: ['aaaa', 'bbbb']
-    },
-    visualMap: {
-      show: false,
-      max: 100,
-      inRange: {
-        symbolSize: [20, 70]
-      }
-    },
-    series: [{
-      type: 'scatter',
-      name: 'aaaa',
-      data: data
-    },
-    {
-      name: 'bbbb',
-      type: 'scatter',
-      data: data2
-    }
-    ],
-    animationDelay: function (idx) {
-      return idx * 50;
-    },
-    animationEasing: 'elasticOut'
-  };
-}
+});
