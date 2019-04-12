640e6e1f8956e1beaf54326733603a795b36ed2a
luow <429175116@qq.com>
2019-04-12 09:51:05 +0800

    提交新增产品，零件页

--- dowhere_app/pages/newComponents/newComponents.js
+++ dowhere_app/pages/newComponents/newComponents.js
@@ -1,11 +1,17 @@
-// pages/newComponents/newComponents.js
+// pages/newProduct/newProduct.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: null,
+    code: '',
+    image: '',
+    imgSize: 0,
+    title: '',
+    adImage: '',
   },
 
   /**
@@ -14,53 +20,94 @@ Page({
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  previewImage(e) {
+    var current = e.target.dataset.src
+    wx.previewImage({
+      current: current,
+      urls: this.data.pics
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  chooseImage() {
+    wx.chooseImage({
+      // 最多可以选择的图片张数，默认9
+      count: 1,
+      // original 原图，compressed 压缩图，默认二者都有
+      sizeType: ['original', 'compressed'],
+      // album 从相册选图，camera 使用相机，默认二者都有
+      sourceType: ['album', 'camera'],
+      success: res => {
+        this.setData({
+          imgSize: res.tempFiles[0].size
+        })
+        // 判断图片大小1024000
+        if (this.imgSize > 1024000) {
+          wepy.showModal({
+            title: '',
+            content: '上传图片大小不可大于1兆(1M)',
+            showCancel: false
+          })
+          // return后不会执行添加图片对象部分，form表单请求出可以不做校验
+          return
+        }
+        var tempFilePaths = res.tempFilePaths
+        // 获取图片路径
+        this.setData({
+          image: tempFilePaths[0]
+        })
+      },
+      fail() {},
+      complete() {}
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  setAdvertising(event) {
+    // 产品名称
+    
+    let newAdvertisingName = event.detail.value.name
+    let newImage = this.data.image
+    console.log(newAdvertisingName)
+    console.log(newImage)
+    if (newAdvertisingName == '') {
+      wepy.showModal({
+        title: '',
+        content: '请输入产品名称',
+        showCancel: false
+      })
+      return
+    }
+    if (newImage == '') {
+      wepy.showModal({
+        title: '',
+        content: '请选择图片',
+        showCancel: false
+      })
+      return
+    }
+    // 发送请求,发布广告
+    wx.uploadFile({
+      url: `${app.globalData.requestUrl}/api/add_goods`,
+      method: 'POST',
+      header: {
+        "Content-Type": "multipart/form-data"
+      },
+      filePath: newImage,
+      name: 'image',
+      formData: {
+        title: newAdvertisingName // 广告名称
+      },
+      success: data => {
+        if (data.statusCode === 201) {
+          
+          return
+        }
+        if (data.statusCode === 400) {
+          data = data.data
+          wepy.showModal({
+            title: '',
+            content: data.message,
+            showCancel: false
+          })
+        }
+      }
+    })
   }
 })
