5d56cd73c238a57c46022ff37b8913e9f9a0caf3
luow <429175116@qq.com>
2019-06-17 09:19:14 +0800

    -

--- dowhere_app/pages/login/login.js
+++ dowhere_app/pages/login/login.js
@@ -7,8 +7,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userName: '',
-    userPaw: '',
+    userName: '18700458359',
+    userPaw: '123123',
     passwordInputType: 'password',
     passwordIcon: 'zhengyan'
   },
